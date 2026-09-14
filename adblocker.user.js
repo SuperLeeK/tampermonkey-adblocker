372c9f6f8fd594370498c9ea727c4e1e2ec5be27
Zero <zero@matchtech.co.kr>
2026-09-14 11:21:35 +0900

    버전 업데이트

--- adblocker.user.js
+++ adblocker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dynamic Ad Blocker
 // @namespace    ADBlocker
-// @version      202609111111
+// @version      202609141121
 // @description  Hides ads dynamically based on selectors from a GitHub Gist URL.
 // @author       Zero
 // @match        *://*/*
